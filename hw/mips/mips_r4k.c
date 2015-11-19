7dc662a82703f55c54a296da749c06391e295f45
Stefan Weil <sw@weilnetz.de>
2015-11-19 08:07:57 +0100

    mips_r4k: Fix broken code for variable endianness support
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

--- hw/mips/mips_r4k.c
+++ hw/mips/mips_r4k.c
@@ -230,7 +230,7 @@ void mips_init(MachineState *machine)
     } else {
         bios_size = -1;
     }
-    g_assert(current_cpu->bigendian == bigendian);
+    g_assert(ENV_GET_CPU(env)->bigendian == bigendian);
     if ((bios_size > 0) && (bios_size <= BIOS_SIZE)) {
         bios = g_new(MemoryRegion, 1);
         memory_region_init_ram(bios, NULL, "mips_r4k.bios", BIOS_SIZE,
@@ -302,8 +302,12 @@ void mips_init(MachineState *machine)
 static
 void mips_r4k_init(MachineState *machine)
 {
-    /* Run MIPS system in big endian mode. */
+    /* Run MIPS system in standard endian mode. */
+#if defined(TARGET_WORDS_BIGENDIAN)
     bigendian = true;
+#else
+    bigendian = false;
+#endif
     mips_init(machine);
 }
 
