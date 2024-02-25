5ee976a5a4fe6beb2d208a6d5183b4b94a2f52b4
Stefan Weil <sw@weilnetz.de>
2024-02-25 17:24:29 +0100

    Clean shell scripts for cross build
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -2,7 +2,7 @@
 
 # GitHub actions - Create QEMU installer for Windows
 
-# Author: Stefan Weil (2020-2023)
+# Author: Stefan Weil (2020-2024)
 
 #~ set -e
 set -x
@@ -20,43 +20,6 @@ ROOTDIR=$PWD
 DISTDIR=$ROOTDIR/dist
 HOST=$ARCH-w64-mingw32
 BUILDDIR=bin/ndebug/$HOST
-
-echo deb http://de.archive.ubuntu.com/ubuntu kinetic main universe | \
-  sudo tee /etc/apt/sources.list.d/kinetic.list
-
-sudo apt-get update
-sudo apt-get install --yes curl make pkg-config
-
-# Install packages.
-sudo apt-get update
-sudo apt-get install --yes --no-install-recommends \
-  mingw-w64-tools ninja-build nsis \
-  gcc libc6-dev \
-  g++-mingw-w64-${ARCH/_/-} gcc-mingw-w64-${ARCH/_/-} \
-  bison flex gettext python3-sphinx texinfo
-
-# Get newer version of mingw-w64-*-dev.
-if test "$ARCH" = "i686"; then
-curl -sS -O http://de.archive.ubuntu.com/ubuntu/pool/universe/m/mingw-w64/mingw-w64-i686-dev_10.0.0-3_all.deb
-sudo dpkg -i mingw-w64-i686-dev_10.0.0-3_all.deb
-else
-curl -sS -O http://de.archive.ubuntu.com/ubuntu/pool/universe/m/mingw-w64/mingw-w64-x86-64-dev_10.0.0-3_all.deb
-sudo dpkg -i mingw-w64-x86-64-dev_10.0.0-3_all.deb
-fi
-
-# Get header files for WHPX API from Mingw-w64 git master.
-if test "$ARCH" != "i686"; then
-(
-sudo mkdir -p /usr/$HOST/sys-include
-cd /usr/$HOST/sys-include
-sudo curl -sS -o winhvemulation.h https://sourceforge.net/p/mingw-w64/mingw-w64/ci/master/tree/mingw-w64-headers/include/winhvemulation.h?format=raw
-sudo curl -sS -o winhvplatform.h https://sourceforge.net/p/mingw-w64/mingw-w64/ci/master/tree/mingw-w64-headers/include/winhvplatform.h?format=raw
-sudo curl -sS -o winhvplatformdefs.h https://sourceforge.net/p/mingw-w64/mingw-w64/ci/master/tree/mingw-w64-headers/include/winhvplatformdefs.h?format=raw
-sudo ln -s winhvemulation.h WinHvEmulation.h
-sudo ln -s winhvplatform.h WinHvPlatform.h
-sudo ln -s winhvplatformdefs.h WinHvPlatformDefs.h
-)
-fi
 
 mkdir -p $DISTDIR
 
--- .github/workflows/pacman.sh
+++ .github/workflows/pacman.sh
@@ -2,7 +2,7 @@
 
 # GitHub actions - Install packages for Windows cross builds
 
-# Author: Stefan Weil (2023)
+# Author: Stefan Weil (2023-2024)
 
 ARCH=$1
 
@@ -13,13 +13,28 @@ else
   MINGW=mingw64
 fi
 
-ROOTDIR=$PWD
-DISTDIR=$ROOTDIR/dist
-HOST=$ARCH-w64-mingw32
-BUILDDIR=bin/ndebug/$HOST
+# Install required packages.
 
 sudo apt-get update
-sudo apt-get install --no-install-recommends -y git libarchive-dev libcurl3-dev libgpgme-dev libssl-dev make meson sudo
+sudo apt-get install --no-install-recommends --yes \
+  git libarchive-dev libcurl3-dev libgpgme-dev libssl-dev make meson sudo \
+  curl make pkg-config \
+  mingw-w64-tools ninja-build nsis \
+  gcc libc6-dev \
+  g++-mingw-w64-${ARCH/_/-} gcc-mingw-w64-${ARCH/_/-} \
+  bison bzip2 flex gettext python3-sphinx python3-venv texinfo
+
+# Get newer version of mingw-w64-*-dev (with pathcch).
+
+if test "$ARCH" = "i686"; then
+curl -sS -O http://de.archive.ubuntu.com/ubuntu/pool/universe/m/mingw-w64/mingw-w64-i686-dev_10.0.0-3_all.deb
+sudo dpkg -i mingw-w64-i686-dev_10.0.0-3_all.deb
+else
+curl -sS -O http://de.archive.ubuntu.com/ubuntu/pool/universe/m/mingw-w64/mingw-w64-x86-64-dev_10.0.0-3_all.deb
+sudo dpkg -i mingw-w64-x86-64-dev_10.0.0-3_all.deb
+fi
+
+# Install pacman.
 
 git clone https://gitlab.archlinux.org/sw/pacman.git
 (
@@ -46,6 +61,8 @@ sudo pacman-key --populate msys2
 sudo pacman -Syu
 
 test -e /etc/mtab || sudo ln -s ../proc/self/mounts /etc/mtab
+
+# Install packages for cross build with pacman.
 
 sudo pacman -S --noconfirm mingw-w64-$ARCH-SDL2 mingw-w64-$ARCH-SDL2_image mingw-w64-$ARCH-asciidoc mingw-w64-$ARCH-cairo mingw-w64-$ARCH-capstone mingw-w64-$ARCH-curl-winssl mingw-w64-$ARCH-cyrus-sasl mingw-w64-$ARCH-gnutls mingw-w64-$ARCH-gtk3 mingw-w64-$ARCH-headers-git mingw-w64-$ARCH-icu mingw-w64-$ARCH-jack2 mingw-w64-$ARCH-leptonica mingw-w64-$ARCH-libarchive mingw-w64-$ARCH-libb2 mingw-w64-$ARCH-libnfs mingw-w64-$ARCH-libslirp mingw-w64-$ARCH-libssh mingw-w64-$ARCH-lz4 mingw-w64-$ARCH-pango mingw-w64-$ARCH-pdcurses mingw-w64-$ARCH-snappy mingw-w64-$ARCH-spice mingw-w64-$ARCH-usbredir mingw-w64-$ARCH-virglrenderer
 
